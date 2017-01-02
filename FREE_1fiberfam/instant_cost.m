c65e3ac618cdeabf52fd8f8ef236d8fdb4d0e176
bruderd <dbruder@Daniel-Bruders-MacBook.local>
2017-01-02 14:47:18 -0500

    testing github versions
    Made a small change to instant_cost.m

--- FREE_1fiberfam/instant_cost.m
+++ FREE_1fiberfam/instant_cost.m
@@ -2,8 +2,8 @@ function [ic, dicdx, dicdu] = instant_cost(x, u, params)
     
     % Desired twist, 1 fiber-family family FREE
     phi_desired = params.phi_desired;
-    ic = 10*(phi_desired - x(5))^2 + 0.01*u^2;
-    dicdx = 10*[0 0 0 0 -2*(phi_desired - x(5))];
+    ic = 50*(phi_desired - x(5))^2 + 0.01*u^2;
+    dicdx = 50*[0 0 0 0 -2*(phi_desired - x(5))];
     dicdu = 2*u*0.01;
     
 %     % Desired pressure
