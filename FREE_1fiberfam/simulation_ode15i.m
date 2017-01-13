9709e2d631606162846b51d1edb5f833a426dc0a
bruderd <dbruder@Daniel-Bruders-MacBook.local>
2017-01-12 23:18:22 -0500

    changed sign of terms in first equation
    Makes sense, but doesn’t match ICRA. Simulation results are not
    satisfactory

--- FREE_1fiberfam/simulation_ode15i.m
+++ FREE_1fiberfam/simulation_ode15i.m
@@ -1,7 +1,7 @@
 %% ODE15i Simulation
 %   Use this to check of the dynamics are broken, or if it's something else
 
-tspan = [0, 0.1];
+tspan = [0, 10];
 x_rest = [0.001, deg2rad(40), 3/16, 5.68]';     % resting point
 x0 = [0.001, deg2rad(40), 3/16, 5.68]';       % initial point
 % x0 = [9.99, 0.67183, 0.21476, 5.8307]';         % ss value taken from ICRA
