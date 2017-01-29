5b12fc5c269b229df6679f6c03ceb50f8011eb76
bruderd <dbruder@Daniel-Bruders-MacBook.local>
2017-01-28 23:11:26 -0500

    More general case 1 implemented
    nicer plot output of optimization as well

--- FREE_2fiberfam/force2diffeq/forcebalance2diffeq_2fib.m
+++ FREE_2fiberfam/force2diffeq/forcebalance2diffeq_2fib.m
@@ -1,7 +1,7 @@
 clear all;
 
 % syms r0 L0 gama0 P gama r L dP dgama dr dL c1 c2 c3 c4 c5 c6
-syms r0 L0 gama0 dP dgama dr dL c1 c2 c3 c4 c5 c6 betta0
+syms r0 L0 gama0 dP dgama dr dL c1 c2 c3 c4 c5 c6 betta0 nrat
 
 assume(r0, 'real')
 assumeAlso(L0, 'real')
@@ -17,6 +17,7 @@ assumeAlso(c3, 'real')
 assumeAlso(c4, 'real')
 assumeAlso(c5, 'real')
 assumeAlso(c6, 'real')
+assumeAlso(nrat, 'real')
 
 
 syms t
@@ -28,7 +29,7 @@ r = sym('r(t)');
 L = sym('L(t)');
 phi = sym('phi(t)');
 
-theta = tan(gama)*L/r;
+% theta = tan(gama)*L/r;
 % phi = (tan(gama)*L/r - tan(gama0)*L0/r0);  %switch the sign to be consistent with Josh's sign convention of twist (1/26/2017)
 
 assumeAlso(P, 'real')
@@ -108,18 +109,36 @@ M_elast = c4 * phi;    % removed (-1) becasue I changed phi up there (1/26/2017)
 %% Case 1: gama,betta>0, gama>betta. Differentiates the system of equations wrt time (2 fibers)
     % New state added: phi = "twist"
    
-T_gama = (2*(pi*P*r^2*cot(gama) + P*r^2*pi*cos((pi*tan(betta))/tan(gama))*cot(betta)))/(sin(gama)*(cos((pi*tan(betta))/tan(gama)) + 1));
-T_betta = (2*(pi*P*r^2*cot(betta) - pi*P*r^2*cot(gama)))/(sin(betta) + cos((pi*tan(betta))/tan(gama))*sin(betta));
-theta_gama0 = tan(gama0)*L0/r0;
-theta_betta0 = tan(betta0)*L0/r0;
+% T_gama = (2*(pi*P*r^2*cot(gama) + P*r^2*pi*cos((pi*tan(betta))/tan(gama))*cot(betta)))/(sin(gama)*(cos((pi*tan(betta))/tan(gama)) + 1));
+% T_betta = (2*(pi*P*r^2*cot(betta) - pi*P*r^2*cot(gama)))/(sin(betta) + cos((pi*tan(betta))/tan(gama))*sin(betta));
+% theta_gama0 = tan(gama0)*L0/r0;
+% theta_betta0 = tan(betta0)*L0/r0;
+% 
+% force_balance = diff( 0 == P*pi*r^2 - (T_gama*cos(gama) + T_betta*cos(betta)) + F_elast ,  t);   
+% torque_balance = diff( 0 == (T_gama*sin(gama) + T_betta*sin(betta)) + M_elast ,  t);               
+% geometry_constraint1 = diff( 0 == L/cos(gama) - r*(theta_gama0 + phi)/sin(gama) ,  t);
+% geometry_constraint2 = diff( 0 == L/cos(betta) - r*(theta_betta0 + phi)/sin(betta) ,  t);
+% geometry_constraint3 = diff( 0 == (L/r)*tan(gama) - (L0/r0)*tan(gama0) - phi ,  t);
+% 
+% 
+% System_2fib = [force_balance; torque_balance; geometry_constraint1; geometry_constraint2; geometry_constraint3];
+
+
+%% (UPDATE from typo) Case 1: gama,betta>0, gama>betta. Differentiates the system of equations wrt time (2 fibers) (1/28/2017)
+    % New state added: phi = "twist"
+   
+T_gama = (4*P*r^2*pi*(2*cot(gama) - cot(betta) + cos((pi*tan(betta))/tan(gama))*cot(betta)))/(sin(gama)*(cos((pi*tan(betta))/tan(gama)) - nrat + cos(pi*(nrat - tan(gama)/tan(betta))) - cos((pi*tan(betta))/tan(gama))*cos(pi*(nrat - tan(gama)/tan(betta))) + nrat*cos((pi*tan(betta))/tan(gama)) + 3));
+T_betta = (4*P*r^2*pi*(2*cot(betta) - cot(gama) + cos(pi*(nrat - tan(gama)/tan(betta)))*cot(gama) - nrat*cot(gama)))/(sin(betta)*(cos((pi*tan(betta))/tan(gama)) - nrat + cos(pi*(nrat - tan(gama)/tan(betta))) - cos((pi*tan(betta))/tan(gama))*cos(pi*(nrat - tan(gama)/tan(betta))) + nrat*cos((pi*tan(betta))/tan(gama)) + 3));
+theta_gama0 = -tan(gama0)*L0/r0;     % (-) fixes sign convention (1/28/2017)
+theta_betta0 = -tan(betta0)*L0/r0;   % (-) fixes sign convention (1/28/2017)
+theta_gama = -tan(gama)*L/r;       % (-) fixes sign convention (1/28/2017)
 
 force_balance = diff( 0 == P*pi*r^2 - (T_gama*cos(gama) + T_betta*cos(betta)) + F_elast ,  t);   
 torque_balance = diff( 0 == (T_gama*sin(gama) + T_betta*sin(betta)) + M_elast ,  t);               
 geometry_constraint1 = diff( 0 == L/cos(gama) - r*(theta_gama0 + phi)/sin(gama) ,  t);
 geometry_constraint2 = diff( 0 == L/cos(betta) - r*(theta_betta0 + phi)/sin(betta) ,  t);
-geometry_constraint3 = diff( 0 == (L/r)*tan(gama) - (L0/r0)*tan(gama0) - phi ,  t);
+geometry_constraint3 = diff( 0 == (theta_gama - theta_gama0) - phi ,  t);        
 
 
 System_2fib = [force_balance; torque_balance; geometry_constraint1; geometry_constraint2; geometry_constraint3];
-
 
