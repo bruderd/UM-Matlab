85cbb46753f8c5faa7de65cfb8c68bd768ed6dec
bruderd <bruderd@umich.edu>
2018-02-15 15:40:52 -0500

    Moved some files and added some new measured data

--- TRI/IROS2018/main.m
+++ TRI/IROS2018/main.m
@@ -4,25 +4,25 @@ clear
 %% Set parameter values
 % Set the values of system parameters
 Gama = deg2rad([40, -40, -89]);
-R = (10e-3)/2 * ones(1,3); % R = 0.009525/2 * ones(1,3);
-L = 0.1 * ones(1,3);
+R = (9e-3)/2 * ones(1,3); % R = 0.009525/2 * ones(1,3);
+L = 0.10 * ones(1,3);
 d = zeros(3,3);
 p = [0,0,0 ; 0,0,0 ; 1,1,1];
-Pmax = [103.421e3, 103.421e3, 103.421e3];   % Pa = 15psi
+Pmax = (1/0.14503) * 1e3 * [15 15 15];   % Pa = 15psi
 params = setParams(Gama, R, L, d, p, Pmax);
 
-% Set the value of test parameters
-testParams = struct;
-testParams.Psteps = 4;     % how finely to break up Pmax
-testParams.stest = [0, 5, 0, 5];   % mm
-testParams.wtest = [0, 0, 20, 20];    % deg
-testParams.TRmax = 103.421e3;   % Pa = 15psi
-
-% % Much smaller set of points for debugging
-% testParams.Psteps = 1;     % how finely to break up Pmax
-% testParams.stest = [0, 5];   % mm
-% testParams.wtest = [0, 20];    % deg
+% % Set the value of test parameters
+% testParams = struct;
+% testParams.Psteps = 4;     % how finely to break up Pmax
+% testParams.stest = [0, 5, 0, 5];   % mm
+% testParams.wtest = [0, 0, 20, 20];    % deg
 % testParams.TRmax = 103.421e3;   % Pa = 15psi
+
+% Much smaller set of points for debugging
+testParams.Psteps = 1;     % how finely to break up Pmax
+testParams.stest = [0, 5];   % mm
+testParams.wtest = [0, 20];    % deg
+testParams.TRmax = 103.421e3;   % Pa = 15psi
 
 %% Calculate the force zonotope for each test configuration
 for i = 1:length(testParams.stest)
