48ccdb00f4be7e879c140678b79755e4208ea10c
bruderd <bruderd@umich.edu>
2018-02-18 14:26:27 -0500

    Changes FREE parameters and saved file of manually measured data points

---
+++ TRI/IROS2018/manualFM.m
@@ -0,0 +1,5 @@
+% record a set of force measurements I took manually so that I can plot
+% them on top of zonotope.
+
+FMnew(:,1) = -[-0.008, -4.510, -0.125, 4.5, -0.206, 4.464, 9.5, -0.257, -1.310, -1.335, -0.380, 4.75, 4.6]';
+FMnew(:,2) = [0, 0, 0, -0.08, 0, 0.07, 0, 0, -0.08, 0.08, 0, 0.08, -0.08]';
