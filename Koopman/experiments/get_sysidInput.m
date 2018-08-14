ca6587b6561b7e9f4e030f3975dac90fd07461ec
bruderd <bruderd@umich.edu>
2018-08-14 18:46:06 -0400

    started writing code to run on experimental rig machine with labview

---
+++ Koopman/experiments/get_sysidInput.m
@@ -0,0 +1,9 @@
+function u = get_sysidInput( t, params )
+%get_sysidInput: calculate the input signal (in volts) to the TR pressure
+%regulators at each timestep. Meant to sweep over most of the control space
+%   Detailed explanation goes here
+
+
+
+end
+
