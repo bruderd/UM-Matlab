026567ae0f61c973947cf824c80127acb0fca2fd
bruderd <bruderd@umich.edu>
2019-04-04 18:15:35 -0400

    Been messing around with val_liftedSys_wKalman trying a bunch of stuff that doesn't work.

---
+++ Koopman/ver9-Koopman_loads/fakeSystems/armloadv2_sim.m
@@ -0,0 +1,40 @@
+% armloadv2_sim.m
+%   Main script for simulating a 1dof arm with load attached system
+
+%% set double pendulum parameters
+params = struct;
+
+% system parameters
+params.name = 'armloadv2_100s_load10';
+params.n = 3;   % number of states (load is the third state)
+params.p = 1;   % number of inputs
+
+% physical parameters
+params.M = 1;
+params.K = 20;
+params.L = 1;
+params.D = -10;
+params.g = 9.81;
+params.load = 10;    % LOAD ATTACHED TO END EFFECTOR
+
+% simulation parameters
+params.Ts = 0.02;
+params.tf = 100;
+params.x0 = [ 0, 0, params.load]';
+
+% step/ramp input parameters
+params.steplen = 1;  % duration of each step
+params.steps = pi * rand(2000, params.p);   % random list of 1000 step inputs between 0 and pi
+% params.steps = pi/2 * ones(2000,params.p);
+params.tconstant = 2;
+
+% other params
+params.saveon = true;
+
+%% simulate double pendulum
+data = gen_fakeData( params.name, @armload_vf, @armload_input, params );
+
+%% save function that evaluates dynamics with these system parameters
+x = sym('x' , [params.n , 1]);
+u = sym('u' , [params.p , 1]);
+symbolic_dynamics = armload_vf(x, u, params);
