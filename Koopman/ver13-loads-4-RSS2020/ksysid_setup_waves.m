9b8c9b8745cb5ee4bb218aeea1f18da75f940cf2
bruderd <bruderd@umich.edu>
2020-01-03 13:29:23 -0500

    created sysid_setup_waves to perform sysid on waves without a gui

---
+++ Koopman/ver13-loads-4-RSS2020/ksysid_setup_waves.m
@@ -0,0 +1,50 @@
+% ksysid_setup_waves
+%
+% Creates a sysid class and walks through all of the steps of building a
+% model from data, validating its performance, and saving it (if desired)
+%
+% Modified to work on waves by removing things that require a gui such as
+% 'uigetfile', ...
+
+
+%% gather training data (need to prepare data file before running this)
+
+% % load in data file(s)
+data4sysid = load([ 'datafiles' , filesep , 'softarm_3mods_diffvaldata_train-5_val-5_2020-01-03_12-03.mat' ]);  % Xun's first loaded tests
+
+
+%% construct sysid class
+ksysid = ksysid( data4sysid, ...
+        'model_type' , 'linear' ,...    % model type (linear or nonlinear)
+        'obs_type' , { 'poly' } ,...    % type of basis functions
+        'obs_degree' , [ 1 ] ,...       % "degree" of basis functions
+        'snapshots' , Inf ,...          % Number of snapshot pairs
+        'lasso' , [ Inf ] ,...           % L1 regularization term
+        'delays' , 0 ,...               % Numer of state/input delays
+        'loaded' , true);             % Does system include loads?
+
+    
+%% train model(s)
+ksysid = ksysid.train_models;
+
+
+%% validate model(s)
+% could also manually do this for one model at a time
+
+results = cell( size(ksysid.candidates) );    % store results in a cell array
+err = cell( size(ksysid.candidates) );    % store error in a cell array 
+
+if iscell(ksysid.candidates)
+    for i = 1 : length(ksysid.candidates)
+        [ results{i} , err{i} ] = ksysid.valNplot_model( i );
+    end
+else
+    [ results{1} , err{1} ] = ksysid.valNplot_model;
+end
+    
+
+%% save model(s)
+
+ksysid.save_class;
+
+
